a1483139ad51eecef5b1aa74a03c13057ac18aa2
Keith Winstein <keithw@cs.stanford.edu>
2017-05-11 21:56:34 -0700

    Import musl

--- fetch-submodules.sh
+++ fetch-submodules.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -xe
 
-git submodule init gcc
+git submodule init
 mkdir -p $(git rev-parse --git-dir)/modules
-git clone --depth 1 --branch gcc-6_3_0-release --separate-git-dir $(git rev-parse --git-dir)/modules/gcc https://github.com/gcc-mirror/gcc gcc
+
+# shallow clone of gcc at tag gcc-6_3_0-release
+git clone --depth 1 --branch gcc-6_3_0-release --separate-git-dir $(git rev-parse --git-dir)/modules/gcc https://github.com/gcc-mirror/gcc
+
+# shallow clone of musl at tag v1.1.16
+git clone --depth 1 --branch v1.1.16 --separate-git-dir $(git rev-parse --git-dir)/modules/musl git://git.musl-libc.org/musl
