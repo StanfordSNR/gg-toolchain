668aec68c5bf85f8e5f5fcabd21ff582b96f9316
Keith Winstein <keithw@cs.stanford.edu>
2017-05-20 12:18:57 -0700

    Update gcc to 7.1

--- fetch-submodules.sh
+++ fetch-submodules.sh
@@ -3,8 +3,8 @@
 git submodule init
 mkdir -p $(git rev-parse --git-dir)/modules
 
-# shallow clone of gcc at tag gcc-6_3_0-release
-git clone --depth 1 --branch gcc-6_3_0-release --separate-git-dir $(git rev-parse --git-dir)/modules/gcc https://github.com/gcc-mirror/gcc
+# shallow clone of gcc at tag gcc-7_1_0-release
+git clone --depth 1 --branch gcc-7_1_0-release --separate-git-dir $(git rev-parse --git-dir)/modules/gcc https://github.com/gcc-mirror/gcc
 
 # shallow clone of musl at tag v1.1.16
 git clone --depth 1 --branch v1.1.16 --separate-git-dir $(git rev-parse --git-dir)/modules/musl git://git.musl-libc.org/musl
