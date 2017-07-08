1cb150b8cf344e2141fa2fbfea4adda12f80f6e0
Keith Winstein <keithw@cs.stanford.edu>
2017-07-07 21:28:36 -0700

    build-script now builds gg-gcc

--- fetch-submodules.sh
+++ fetch-submodules.sh
@@ -8,3 +8,6 @@ git clone --depth 1 --branch gcc-7_1_0-release --separate-git-dir $(git rev-pars
 
 # shallow clone of musl at tag v1.1.16
 git clone --depth 1 --branch v1.1.16 --separate-git-dir $(git rev-parse --git-dir)/modules/musl git://git.musl-libc.org/musl
+
+# fetch gcc dependences
+cd gcc && ./contrib/download_prerequisites
--- make-binutils-symlinks.sh
+++ make-binutils-symlinks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -xe
 
-ln -s /usr/bin/x86_64-linux-gnu-strip inst/bin/x86_64-linux-musl-strip
-ln -s /usr/bin/x86_64-linux-gnu-ranlib inst/bin/x86_64-linux-musl-ranlib
-ln -s /usr/bin/x86_64-linux-gnu-nm inst/bin/x86_64-linux-musl-nm
-ln -s /usr/bin/x86_64-linux-gnu-ld inst/bin/x86_64-linux-musl-ld
-ln -s /usr/bin/x86_64-linux-gnu-as inst/bin/x86_64-linux-musl-as
-ln -s /usr/bin/x86_64-linux-gnu-ar inst/bin/x86_64-linux-musl-ar
+ln -sf /usr/bin/x86_64-linux-gnu-strip inst/bin/x86_64-linux-musl-strip
+ln -sf /usr/bin/x86_64-linux-gnu-ranlib inst/bin/x86_64-linux-musl-ranlib
+ln -sf /usr/bin/x86_64-linux-gnu-nm inst/bin/x86_64-linux-musl-nm
+ln -sf /usr/bin/x86_64-linux-gnu-ld inst/bin/x86_64-linux-musl-ld
+ln -sf /usr/bin/x86_64-linux-gnu-as inst/bin/x86_64-linux-musl-as
+ln -sf /usr/bin/x86_64-linux-gnu-ar inst/bin/x86_64-linux-musl-ar
