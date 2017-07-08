31e006bd6e5beae062b0081efb48216df7460371
Keith Winstein <keithw@cs.stanford.edu>
2017-07-07 19:34:28 -0700

    make-binutils-symlinks.sh: add script to make minimal musl binutils

---
+++ make-binutils-symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -xe
+
+ln -s /usr/bin/x86_64-linux-gnu-strip inst/bin/x86_64-linux-musl-strip
+ln -s /usr/bin/x86_64-linux-gnu-ranlib inst/bin/x86_64-linux-musl-ranlib
+ln -s /usr/bin/x86_64-linux-gnu-nm inst/bin/x86_64-linux-musl-nm
+ln -s /usr/bin/x86_64-linux-gnu-ld inst/bin/x86_64-linux-musl-ld
+ln -s /usr/bin/x86_64-linux-gnu-as inst/bin/x86_64-linux-musl-as
+ln -s /usr/bin/x86_64-linux-gnu-ar inst/bin/x86_64-linux-musl-ar
