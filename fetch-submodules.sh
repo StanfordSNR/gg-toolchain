18f6b0502e33e041f6210aa3981784f6440bbbab
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-08 16:32:44 -0700

    Use gg branch instead of master for libgg.

--- fetch-submodules.sh
+++ fetch-submodules.sh
@@ -6,8 +6,8 @@ mkdir -p $(git rev-parse --git-dir)/modules
 # shallow clone of gcc at tag gcc-7_1_0-release
 git clone --depth 1 --branch gcc-7_1_0-release --separate-git-dir $(git rev-parse --git-dir)/modules/gcc https://github.com/gcc-mirror/gcc
 
-# shallow clone of libgg (master)
-git clone --depth 1 --branch master --separate-git-dir $(git rev-parse --git-dir)/modules/libgg https://github.com/stanfordsnr/libgg
+# shallow clone of libgg (gg branch)
+git clone --depth 1 --branch gg --separate-git-dir $(git rev-parse --git-dir)/modules/libgg https://github.com/stanfordsnr/libgg
 
 # fetch gcc dependences
 cd gcc && ./contrib/download_prerequisites
