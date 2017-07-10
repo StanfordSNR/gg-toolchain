60b471e25dcd36737a7ec12b8f9a24fdb9acd23b
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-10 10:12:04 -0700

    Remove unused scripts.

--- make-binutils-symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -xe
-
-ln -sf /usr/bin/x86_64-linux-gnu-strip inst/bin/x86_64-linux-musl-strip
-ln -sf /usr/bin/x86_64-linux-gnu-ranlib inst/bin/x86_64-linux-musl-ranlib
-ln -sf /usr/bin/x86_64-linux-gnu-nm inst/bin/x86_64-linux-musl-nm
-ln -sf /usr/bin/x86_64-linux-gnu-ld inst/bin/x86_64-linux-musl-ld
-ln -sf /usr/bin/x86_64-linux-gnu-as inst/bin/x86_64-linux-musl-as
-ln -sf /usr/bin/x86_64-linux-gnu-ar inst/bin/x86_64-linux-musl-ar
