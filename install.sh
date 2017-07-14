4e315b4fdd8480ab5a9f954b502da4fcec7e64c8
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-14 15:20:52 -0700

    install.sh: don't try to generate the header file.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 SRCDIR=`pwd`
 DESTDIR=${SRCDIR}/bin
 INSTDIR=${SRCDIR}/inst/usr/bin
-INCLUDEDIR=${SRCDIR}/include
 GCC_INSTDIR=${SRCDIR}/inst/usr/libexec/gcc/x86_64-linux-gnu/7.1.0
 
 mkdir -p ${DESTDIR}
@@ -22,6 +21,3 @@ for exe in $(ls ${DESTDIR})
 do
   strip ${DESTDIR}/${exe}
 done
-
-mkdir -p ${INCLUDEDIR}
-./generate-header.py >${INCLUDEDIR}/toolchain.hh
