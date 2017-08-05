5baca3e20b0a0f926855609e1972a712fc32d685
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-04 17:25:28 -0700

    Add g++ and cc1plus

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ GCC_INSTDIR=${SRCDIR}/inst/usr/libexec/gcc/x86_64-linux-gnu/7
 
 mkdir -p ${DESTDIR}
 
-for exe in gcc ld as ar ranlib nm strip as
+for exe in g++ gcc ld as ar ranlib nm strip as
 do
   cp ${INSTDIR}/gg-${exe} ${DESTDIR}/${exe}
 done
 
-for exe in cc1 collect2
+for exe in cc1 cc1plus collect2
 do
   cp ${GCC_INSTDIR}/${exe} ${DESTDIR}/${exe}
 done
